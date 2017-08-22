6936601afa95b11bdf1ca5c49d7c801d41c6d8e2
Tharaka Ranatunga <tharaka.14@cse.mrt.ac.lk>
2017-08-22 09:50:47 +0530

    adding jenkinsfile

--- src/main/java/com/mycompany/hellomaven/HelloMaven.java
+++ src/main/java/com/mycompany/hellomaven/HelloMaven.java
@@ -10,5 +10,12 @@ package com.mycompany.hellomaven;
  * @author tharaka_ra
  */
 public class HelloMaven {
-    
+    public static void main(String[] args) {
+        int k;
+	for(int j=0; j<2; j++){
+		for(int i=0; i<=10; i++){
+           		System.out.println("Hello Jenkins" + i);
+        	}
+	}
+    }
 }
