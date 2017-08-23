0b9eb9cd5d0c0d52a35d8efe185d337164a2429e
Tharaka Ranatunga <tharaka.14@cse.mrt.ac.lk>
2017-08-23 11:22:53 +0530

    including bugs

--- src/main/java/com/mycompany/hellomaven/HelloMaven.java
+++ src/main/java/com/mycompany/hellomaven/HelloMaven.java
@@ -1,10 +1,17 @@
 
 package com.mycompany.hellomaven;
 
+import java.math.BigDecimal;
+
 public class HelloMaven {
     public static void main(String[] args) {
-        int k;
-	for(int j=0; j<2; j++){
+        
+        double d = 1.1;
+
+        BigDecimal bd1 = new BigDecimal(d); // Noncompliant; see comment above
+        BigDecimal bd2 = new BigDecimal(1.1); // Noncompliant; same result
+	
+        for(int j=0; j<2; j++){
 		for(int i=0; i<=10; i++){
            		System.out.println("Hello Jenkins" + i);
         	}
